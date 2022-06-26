557d3e4163f25f3a6c2e52038299c0da601831e1
Helio Albano de Oliveira <helio.albano@mercadolivre.com>
2022-06-26 09:09:35 -0300

    feat(catalog): start rest validations

--- src/main/java/me/helioalbano/biblioteca/catalog/infra/controller/CatalogController.java
+++ src/main/java/me/helioalbano/biblioteca/catalog/infra/controller/CatalogController.java
@@ -1,15 +1,18 @@
 package me.helioalbano.biblioteca.catalog.infra.controller;
 
 import me.helioalbano.biblioteca.catalog.infra.controller.dto.CreateBookRequest;
-import me.helioalbano.biblioteca.catalog.infra.controller.dto.CreateBookResponse;
 import me.helioalbano.biblioteca.catalog.usecase.addnewbook.AddNewBook;
 import me.helioalbano.biblioteca.catalog.usecase.addnewbook.dto.AddNewBookInput;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("catalog")
@@ -23,9 +26,10 @@ class CatalogController {
     }
 
     @PostMapping(path = "books")
-    public ResponseEntity<CreateBookResponse> createBook(@RequestBody CreateBookRequest book) {
+    public ResponseEntity<Void> createBook(@Valid @RequestBody CreateBookRequest book) {
         var input = modelMapper.map(book, AddNewBookInput.class);
         addNewBook.execute(input);
-        return ResponseEntity.ok(new CreateBookResponse(input.getTitle()));
+        var location = URI.create("/catalog/books/TODO");
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/java/me/helioalbano/biblioteca/catalog/infra/controller/dto/CreateBookRequest.java
+++ src/main/java/me/helioalbano/biblioteca/catalog/infra/controller/dto/CreateBookRequest.java
@@ -2,7 +2,10 @@ package me.helioalbano.biblioteca.catalog.infra.controller.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CreateBookRequest {
+    @NotNull(message = "The book title must be informed")
     private String title;
 }
